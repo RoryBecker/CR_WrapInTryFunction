63c63a5df55c5ee2ee5690fbe5f0aec01961d3f0
Rory Becker <RoryBecker@gmail.com>
2013-07-05 10:51:49 +0100

    Initial Load

---
+++ CR_WrapInTryFunction/PlugIn1.cs
@@ -0,0 +1,94 @@
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.CodeRush.Core;
+using DevExpress.CodeRush.PlugInCore;
+using DevExpress.CodeRush.StructuralParser;
+
+namespace CR_WrapInTryFunction
+{
+    public partial class PlugIn1 : StandardPlugIn
+    {
+        // DXCore-generated code...
+        #region InitializePlugIn
+        public override void InitializePlugIn()
+        {
+            base.InitializePlugIn();
+            registerWrapInTryFunction();
+        }
+        #endregion
+        #region FinalizePlugIn
+        public override void FinalizePlugIn()
+        {
+            //
+            // TODO: Add your finalization code here.
+            //
+
+            base.FinalizePlugIn();
+        }
+        #endregion
+        public void registerWrapInTryFunction()
+        {
+            DevExpress.CodeRush.Core.CodeProvider WrapInTryFunction = new DevExpress.CodeRush.Core.CodeProvider(components);
+            ((System.ComponentModel.ISupportInitialize)(WrapInTryFunction)).BeginInit();
+            WrapInTryFunction.ProviderName = "Wrap In Try Function"; // Should be Unique
+            WrapInTryFunction.DisplayName = "Wrap In Try Function";
+            WrapInTryFunction.CheckAvailability += WrapInTryFunction_CheckAvailability;
+            WrapInTryFunction.Apply += WrapInTryFunction_Apply;
+            ((System.ComponentModel.ISupportInitialize)(WrapInTryFunction)).EndInit();
+        }
+        private void WrapInTryFunction_CheckAvailability(Object sender, CheckContentAvailabilityEventArgs ea)
+        {
+
+            Method activeMethod = CodeRush.Source.ActiveMethod;
+            ea.Available = activeMethod != null && activeMethod.MethodType == MethodTypeEnum.Function;
+        }
+
+        private void WrapInTryFunction_Apply(Object sender, ApplyContentEventArgs ea)
+        {
+            // INITIALIZE
+            Class activeClass = CodeRush.Source.ActiveClass;
+            Method activeMethod = CodeRush.Source.ActiveMethod;
+            var builder = new ElementBuilder();
+
+
+            var method = builder.AddMethod(activeClass, "bool", "Try" + activeMethod.Name);
+
+            // PARAMS
+            foreach (Param param in activeMethod.Parameters)
+            {
+                method.Parameters.Add(new Param(param.ParamType, param.Name));
+            }
+            Param resultParam = builder.BuildParameter(activeMethod.GetTypeName(), "result", ArgumentDirection.Out);
+            method.Parameters.Add(resultParam);
+
+
+            // METHOD CALL
+            var arguments = new List<string>();
+            foreach (IParameterElement SourceParam in activeMethod.Parameters)
+            {
+                arguments.Add(SourceParam.Name);
+            }
+
+            var methodCall = builder.BuildMethodCall(activeMethod.Name, arguments.ToArray());
+            
+            var Try = builder.AddTry(method);
+            Try.AddNode(builder.BuildAssignment("result", methodCall));
+            Try.AddNode(builder.BuildReturn("true"));
+
+            var ExCatch = builder.AddCatch(method);
+            ExCatch.AddNode(builder.BuildAssignment("result", CodeRush.Language.GetNullReferenceExpression()));
+            ExCatch.AddNode(builder.BuildReturn("false"));
+
+            // RENDER METHOD
+            activeClass.AddNode(method);
+            var Code = CodeRush.CodeMod.GenerateCode(method, false);
+            int LastLine = activeMethod.Range.End.Line;
+            var newMethodRange = CodeRush.Documents.ActiveTextDocument.InsertText(new SourcePoint(LastLine+1,1), Code);
+            CodeRush.Documents.Format(newMethodRange);
+        }
+    }
+}
---
+++ CR_WrapInTryFunction/PlugIn1VsixExtension.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.Composition;
+using DevExpress.CodeRush.Common;
+
+namespace CR_WrapInTryFunction
+{
+    [Export(typeof(IVsixPluginExtension))]
+    public class CR_WrapInTryFunctionExtension : IVsixPluginExtension { }
+}
